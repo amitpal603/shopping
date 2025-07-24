2167e139fafd956c6d06ef171bb90b6f91acef8c
amitpal603-ops <ap036776@gmail.com>
2025-07-24 17:31:45 +0530

    changes modify

--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Contact() {
+
+  const [data,setData] = useState([{name: "",product: "",msg: ""}])
+
+
+  const HandleChange = (e) => {
+    e.preventDefault()
+    const {name,value} = e.target
+
+    setData((prev) =>({
+      ...prev,[name] : value
+    }))
+    
+  }
+  
   return (
     <div className="flex justify-center items-center flex-col mt-10 gap-10 px-4 ">
       <h1 className="w-60 font-bold rounded shadow-lg bg-purple-400 text-3xl text-center p-4 text-white">
         Contact
       </h1>
 
-      <div className="w-full max-w-md  p-6 rounded-lg shadow-lg space-y-5">
+      <div
+      className="w-full max-w-md  p-6 rounded-lg shadow-lg space-y-5">
         <div>
           <label className="block font-semibold text-lg mb-2" htmlFor="name">
             Name
@@ -16,6 +31,9 @@ function Contact() {
             id="name"
             className="w-full h-12 px-4 border-2 border-gray-400 rounded focus:outline-none focus:border-purple-500"
             type="text"
+            value={data.name}
+            name='name'
+            onChange={HandleChange}
             placeholder="Enter your name..."
           />
         </div>
@@ -25,9 +43,12 @@ function Contact() {
             Product
           </label>
           <input
-            id="email"
+            id="product"
             className="w-full h-12 px-4 border-2 border-gray-400 rounded focus:outline-none focus:border-purple-500"
-            type="email"
+            type="text"
+            value={data.product}
+            name='product'
+            onChange={HandleChange}
             placeholder="Enter your Product..."
           />
         </div>
@@ -38,12 +59,17 @@ function Contact() {
           </label>
           <textarea
             id="message"
+            value={data.msg}
+            name='msg'
+            onChange={HandleChange}
             className="w-full h-32 px-4 py-2 border-2 border-gray-400 rounded resize-none focus:outline-none focus:border-purple-500"
             placeholder="Write your message..."
           />
         </div>
 
-        <button className="w-full bg-purple-500 text-white py-3 font-bold rounded hover:bg-purple-600 transition duration-300">
+        <button 
+        type="submit"
+        className="w-full bg-purple-500 text-white py-3 font-bold rounded hover:bg-purple-600 transition duration-300">
           Submit
         </button>
       </div>
